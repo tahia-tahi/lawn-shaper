e931500e08db277c2a79c23714910e0ace84bc8e
tahia-tahi <devpro526@gmail.com>
2025-05-20 04:32:10 +0600

    routes created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,3 +1,45 @@
 import { createBrowserRouter } from "react-router";
+import Home from "../Pages/Home";
+import ExploreGardeners from "../Pages/ExploreGardeners";
+import MyTips from "../Pages/MyTips";
+import ShareTip from "../Pages/ShareTip";
+import SignUp from "../Pages/SignUp";
+import LogIn from "../Pages/LogIn"
+import ErrorPage from "../Pages/ErrorPage";
 
-export const router = createBrowserRouter([]);
+export const router = createBrowserRouter([
+    {
+        path:'/',
+        Component:MathMLElement,
+        children:[
+            {
+                path:'/',
+                Component:Home
+            },
+            {
+                path:'/explore',
+                Component:ExploreGardeners
+            },
+            {
+                path:'/mytips',
+                Component:MyTips
+            },
+            {
+                path:'/sharetip',
+                Component:ShareTip
+            },
+            {
+                path:'/login',
+                Component:LogIn
+            },
+            {
+                path:'/signup',
+                Component:SignUp
+            },
+            {
+                path: '/error',
+                Component:ErrorPage
+            }
+        ]
+    }
+]);
